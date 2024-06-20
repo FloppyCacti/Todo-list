6f52c64005e57d555d33095844986e378628b24d
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-20 18:51:52 -0500

    make function that adds side-bar

--- src/side-bar.js
+++ src/side-bar.js
@@ -1,38 +1,45 @@
-const categories = [categoryFactory('Today'),categoryFactory('Upcoming')]
+function addSideBar(){
+    const categories = [categoryFactory('Today'),categoryFactory('Upcoming')]
 
-function sideBar(){
-    const container = document.querySelector('#container');
+    function sideBar(){
+        const container = document.querySelector('#container');
 
-    const sideBar = document.createElement('div');
-    sideBar.setAttribute('id', 'sideBar');
+        const sideBar = document.createElement('div');
+        sideBar.setAttribute('id', 'sideBar');
 
-    container.appendChild(sideBar);
-}
-
-function buttonToAddCategory(){
-    const sideBar = document.querySelector('#sideBar');
-
-    const addButton = document.createElement('button');
-    addButton.innerHTML = 'Add Category';
-    addButton.style.height = '30px';
-    addButton.style.width = '100%';
-    sideBar.appendChild(addButton);
-}
-
-function categoryFactory(name){
-    return{
-        categoryName: name
+        container.appendChild(sideBar);
     }
-}
 
-function insertCategoriesIntoSideBar(){
-    const container = document.querySelector('#container');
-    const categoryList = docuemnt.createElement('li');
+    function buttonToAddCategory(){
+        const sideBar = document.querySelector('#sideBar');
 
-    categories.forEach(element => {
-        let categoryElement = document.createElement('ul');
-        categoryList.appendChild(categoryElement);
-    });
+        const addButton = document.createElement('button');
+        addButton.innerHTML = 'Add Category';
+        addButton.style.height = '30px';
+        addButton.style.width = '100%';
+        sideBar.appendChild(addButton);
+    }
 
-    container.appendChild(categoryList);
+    function categoryFactory(name){
+        return{
+            categoryName: name
+        }
+    }
+
+    function insertCategoriesIntoSideBar(){
+        const container = document.querySelector('#container');
+        const categoryList = docuemnt.createElement('li');
+
+        categories.forEach(element => {
+            let categoryElement = document.createElement('ul');
+            categoryElement.innerHTML = `${element.categoryName}`;
+            categoryList.appendChild(categoryElement);
+        });
+
+        container.appendChild(categoryList);
+    }
+
+    sideBar();
+    buttonToAddCategory();
+    insertCategoriesIntoSideBar();
 }
