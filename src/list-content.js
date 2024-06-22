3fe4bc8db2231528a2eea70a26bdd2b14f9ae0ec
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-21 20:09:12 -0500

    add task label and button

--- src/list-content.js
+++ src/list-content.js
@@ -8,10 +8,26 @@ export function listContent(){
     listContainer.setAttribute('id', 'listContainer');
 
     const taskToDo = document.createElement('div');
+    const taskToDoLabel = document.createElement('div');
+    taskToDoLabel.classList.add('taskLabel');
+    taskToDoLabel.innerHTML = 'Task To Do';
+    const taskToDoButton = document.createElement('button');
+    taskToDoButton.classList.add('taskButton');
     taskToDo.classList.add('listContentHeader');
 
+    taskToDo.appendChild(taskToDoLabel);
+    taskToDo.appendChild(taskToDoButton);
+
     const taskCompleted = document.createElement('div');
+    const taskCompletedLable = document.createElement('div');
+    taskCompletedLable.classList.add('taskLabel');
+    taskCompletedLable.innerHTML = 'Completed Tasks';
+    const taskCompletedButton = document.createElement('button');
+    taskCompletedButton.classList.add('taskButton');
     taskCompleted.classList.add('listContentHeader');
+
+    taskCompleted.appendChild(taskCompletedLable);
+    taskCompleted.appendChild(taskCompletedButton);
 
     listContainer.appendChild(taskToDo);
     listContainer.appendChild(taskCompleted);
