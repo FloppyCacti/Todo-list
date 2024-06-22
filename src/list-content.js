bcb2d668dfef1b6bc6bc9321188e0110739827d3
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-22 14:45:48 -0500

    nest headers in containers

--- src/list-content.js
+++ src/list-content.js
@@ -7,6 +7,7 @@ export function listContent(){
     const listContainer = document.createElement('div');
     listContainer.setAttribute('id', 'listContainer');
 
+    const taskToDoContainer = document.createElement('div');
     const taskToDoHeader   = document.createElement('div');
     const taskToDoLabel = document.createElement('div');
     taskToDoLabel.classList.add('taskLabel');
@@ -18,7 +19,9 @@ export function listContent(){
 
     taskToDoHeader.appendChild(taskToDoLabel);
     taskToDoHeader.appendChild(taskToDoButton);
+    taskToDoContainer.appendChild(taskToDoHeader);  
 
+    const taskCompletedContainer = document.createElement('div');
     const taskCompletedHeader = document.createElement('div');
     const taskCompletedLable = document.createElement('div');
     taskCompletedLable.classList.add('taskLabel');
@@ -26,9 +29,10 @@ export function listContent(){
     taskCompletedHeader.classList.add('listContentHeader');
 
     taskCompletedHeader.appendChild(taskCompletedLable);
+    taskCompletedContainer.appendChild(taskCompletedHeader);
 
-    listContainer.appendChild(taskToDoHeader);
-    listContainer.appendChild(taskCompletedHeader);
+    listContainer.appendChild(taskToDoContainer);
+    listContainer.appendChild(taskCompletedContainer);
 
     listContent.appendChild(listContainer); 
     container.appendChild(listContent);
