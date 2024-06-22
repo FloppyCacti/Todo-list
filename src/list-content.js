81f33ea9addbe7380bf7c2f402abf0293297e188
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-22 15:08:53 -0500

    put header in sperate function

--- src/list-content.js
+++ src/list-content.js
@@ -1,39 +1,56 @@
 export function listContent(){
-    const container = document.querySelector('#container');
+    function makelistContainer(){
+        const container = document.querySelector('#container');
 
-    const listContent = document.createElement('div');
-    listContent.setAttribute('id', 'listContent');
+        const listContent = document.createElement('div');
+        listContent.setAttribute('id', 'listContent');
 
-    const listContainer = document.createElement('div');
-    listContainer.setAttribute('id', 'listContainer');
+        const listContainer = document.createElement('div');
+        listContainer.setAttribute('id', 'listContainer');
 
-    const taskToDoContainer = document.createElement('div');
-    const taskToDoHeader   = document.createElement('div');
-    const taskToDoLabel = document.createElement('div');
-    taskToDoLabel.classList.add('taskLabel');
-    taskToDoLabel.innerHTML = 'Task To Do';
-    const taskToDoButton = document.createElement('button');
-    taskToDoButton.classList.add('taskButton');
-    taskToDoButton.innerHTML = '➕';
-    taskToDoHeader.classList.add('listContentHeader');
+        const taskCompletedContainer = document.createElement('div');
+        taskCompletedContainer.setAttribute('id', 'taskCompletedContainer');
 
-    taskToDoHeader.appendChild(taskToDoLabel);
-    taskToDoHeader.appendChild(taskToDoButton);
-    taskToDoContainer.appendChild(taskToDoHeader);  
+        const tasktodoContainer = document.createElement('div');
+        tasktodoContainer.setAttribute('id', 'taskToDoContainer');
 
-    const taskCompletedContainer = document.createElement('div');
-    const taskCompletedHeader = document.createElement('div');
-    const taskCompletedLable = document.createElement('div');
-    taskCompletedLable.classList.add('taskLabel');
-    taskCompletedLable.innerHTML = 'Completed Tasks';
-    taskCompletedHeader.classList.add('listContentHeader');
+        listContainer.appendChild(tasktodoContainer);
+        listContainer.appendChild(taskCompletedContainer);
+        listContent.appendChild(listContainer); 
+        container.appendChild(listContent);
+    }
 
-    taskCompletedHeader.appendChild(taskCompletedLable);
-    taskCompletedContainer.appendChild(taskCompletedHeader);
+    function addHeaderToContainer(){
+        const taskToDoContainer = document.querySelector('#taskToDoContainer');
+        taskToDoContainer.setAttribute('id', 'taskToDoContainer');
+        const taskToDoHeader   = document.createElement('div');
+        const taskToDoLabel = document.createElement('div');
+        taskToDoLabel.classList.add('taskLabel');
+        taskToDoLabel.innerHTML = 'Task To Do';
+        const taskToDoButton = document.createElement('button');
+        taskToDoButton.classList.add('taskButton');
+        taskToDoButton.innerHTML = '➕';
+        taskToDoHeader.classList.add('listContentHeader');
 
-    listContainer.appendChild(taskToDoContainer);
-    listContainer.appendChild(taskCompletedContainer);
+        taskToDoHeader.appendChild(taskToDoLabel);
+        taskToDoHeader.appendChild(taskToDoButton);
+        taskToDoContainer.appendChild(taskToDoHeader);  
 
-    listContent.appendChild(listContainer); 
-    container.appendChild(listContent);
+        const taskCompletedContainer = document.querySelector('#taskCompletedContainer');
+        const taskCompletedHeader = document.createElement('div');
+        const taskCompletedLable = document.createElement('div');
+        taskCompletedLable.classList.add('taskLabel');
+        taskCompletedLable.innerHTML = 'Completed Tasks';
+        taskCompletedHeader.classList.add('listContentHeader');
+
+        taskCompletedHeader.appendChild(taskCompletedLable);
+        taskCompletedContainer.appendChild(taskCompletedHeader);
+
+        listContainer.appendChild(taskToDoContainer);
+        listContainer.appendChild(taskCompletedContainer);
+
+    }
+
+    makelistContainer();
+    addHeaderToContainer();
 }
