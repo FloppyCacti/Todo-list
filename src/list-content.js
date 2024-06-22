964ad5f56430eb1497515a8dda05ceff4518a267
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-21 19:35:23 -0500

    add task container

--- src/list-content.js
+++ src/list-content.js
@@ -4,5 +4,18 @@ export function listContent(){
     const listContent = document.createElement('div');
     listContent.setAttribute('id', 'listContent');
 
+    const listContainer = document.createElement('div');
+    listContainer.setAttribute('id', 'listContainer');
+
+    const taskToDo = document.createElement('div');
+    taskToDo.classList.add('listContentHeader');
+
+    const taskCompleted = document.createElement('div');
+    taskCompleted.classList.add('listContentHeader');
+
+    listContainer.appendChild(taskToDo);
+    listContainer.appendChild(taskCompleted);
+
+    listContent.appendChild(listContainer); 
     container.appendChild(listContent);
 }
